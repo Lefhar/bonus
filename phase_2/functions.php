f9c483a60c78e464a3c2937b4d1678b7e65b069e
harold <s.lefebvre907@laposte.net>
2020-12-15 21:07:30 +0100

    bonus algo

--- phase_2/functions.php
+++ phase_2/functions.php
@@ -59,5 +59,41 @@
      * @return string
      */
     function checkBlackBox($word) {
+        $word1 = strtolower($word);//je met toujours tout en minuscule au cas où
+            $arr = array("a" => "b",
+                         "b" => "c",
+                         "c" => "d",
+                         "d" => "e",
+                         "e" => "f",
+                         "f" => "g",
+                         "g" => "h",
+                         "h" => "i",
+                         "i" => "j",
+                         "j" => "k",
+                         "k" => "l",
+                         "l" => "m",
+                         "m" => "n",
+                         "n" => "o",
+                         "o" => "p",
+                         "p" => "q",
+                         "q" => "r",
+                         "r" => "s",
+                         "s" => "t",
+                         "t" => "u",
+                         "u" => "v",
+                         "v" => "w",
+                         "w" => "x",
+                         "x" => "y",
+                         "y" => "z",
+                         "z" => "a",
+                         "3" => "21",
+                         "6" => "42",
+                         "9"=> "63",
+                         "41" => "287"
+                        );// on change les valeurs par un array
+
+                        $retour = strtr($word,$arr);// on fait le remplacement sur word
+        $retour = preg_replace( "/[^A-Za-z0-9]+/", "a", $retour );// maintenant tout ce qui n'est pas alphanumérique on le remplace
+        return $retour;
 
     }
